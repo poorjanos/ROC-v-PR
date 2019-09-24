8e3a54da404dcec1e1a40af8baca964ff97dd835
Janos Poor <poorjanos@gmail.com>
2019-09-24 12:08:19 +0200

    Add annotations to app

--- app.R
+++ app.R
@@ -22,12 +22,26 @@ ui <- fluidPage(
   titlePanel("ROC v PR"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("balanceInput", "Positive Class Rate",
-                  min = 0, max = 0.34, value = 0.34)
+      tags$div(
+        class = "header", checked = NA,
+        tags$strong("Compare ROC and PR Curves"),
+        tags$p("Use slider below to alter skewness in dataset. The app fits a logistic
+                      regression to the adjusted dataset then returns ROC and PR curves on the 
+                      test set."),
+        tags$p("The dataset is the Pima Indians Diabetes dataset available in the mlbench package.
+                      The dataset contains 768 observations and 9 variables. Observations are individuals.
+                      Variables are health-related metrics. 8 variables are continuous predictors and 1
+                      variable is a binary response telling if the individual has diabetes or not."),
+        tags$p("Positive class rate is the proportion of individuals with diabetes in the dataset."),
+        tags$br(),
+        sliderInput("balanceInput", "Positive Class Rate",
+          min = 0, max = 0.34, value = 0.34
+        )
+      )
     ),
     mainPanel(
-        plotOutput("ROC"),
-        plotOutput("PR")
+      plotOutput("ROC"),
+      plotOutput("PR")
     )
   )
 )
@@ -35,40 +49,36 @@ ui <- fluidPage(
 
 # Server ------------------------------------------------------------------------
 server <- function(input, output) {
-  
   rebalanced <- reactive({
 
     # Gen rebalanced dataset
-    label_1_rate = input$balanceInput
-    sample_size = round(label_1_rate * nrow(PimaIndiansDiabetes), 0)
+    label_1_rate <- input$balanceInput
+    sample_size <- round(label_1_rate * nrow(PimaIndiansDiabetes), 0)
 
     sample_ind <- sample(seq_len(nrow(label_1)), size = sample_size)
     label_1_rebalanced <- label_1[sample_ind, ]
 
     data <- rbind(label_0, label_1_rebalanced)
-    
-    #Train test split
+
+    # Train test split
     smp_size <- floor(0.8 * nrow(data))
-    
+
     set.seed(123)
     train_ind <- sample(seq_len(nrow(data)), size = smp_size)
     train <- data[train_ind, ]
     test <- data[-train_ind, ]
-    
-    
+
     # Fit glm
-    fit <- glm(y~., data = train, family = "binomial")
-    
-    
+    fit <- glm(y ~ ., data = train, family = "binomial")
+
     # Predict test
-    preds <- predict(fit, test, type="response")
-    
+    preds <- predict(fit, test, type = "response")
+
     # Return predicted probabilities
     data.frame(preds = preds, y = test$y)
+  })
 
-    })
-  
-    
+
   output$ROC <- renderPlot({
     roc <- roc.curve(scores.class0 = rebalanced()$preds, weights.class0 = rebalanced()$y, curve = TRUE)
     plot(roc)
